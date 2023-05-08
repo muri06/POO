eda1dd74629719207bf66304199eb94bc82405dd
muri06 <130381937+muri06@users.noreply.github.com>
2023-05-08 11:35:55 -0300

    Create ex01.py

---
+++ ListaPoo01/ex01.py
@@ -0,0 +1,33 @@
+1. Um Círculo
+A classe deve ter um atributo raio para armazenar a dimensão da figura e métodos para calcular sua área e sua
+circunferência.
+
+import math
+class Circulo:
+
+
+
+
+2. Uma Disciplina
+A classe deve ter atributos para armazenar o nome da disciplina e as notas dos quatros bimestres e da prova final.
+Os métodos da classe devem permitir calcular a média parcial (com as notas bimestrais) e a média final (com todas
+as notas, caso o aluno tenha ficado em prova final). Considere como média de aprovação o valor 60, notas de 0 a
+100, média parcial ponderada com pesos 2, 2, 3 e 3 e média final como a média aritmética da média parcial com a
+nota da prova final, caso o aluno não seja aprovado por média.
+
+
+3. Uma Viagem
+A classe deve ter atributos para armazenar a distância em km e o tempo gasto em horas e minutos da viagem
+realizada. A classe deve possuir método para calcular a velocidade média atingida na viagem em km/h de acordo
+com a distância e o tempo gasto.
+
+
+4. Uma Entrada de Cinema
+A classe deve ter atributos para armazenar o dia e o horário de uma sessão de cinema e métodos para calcular o
+valor da entrada inteira e da meia-entrada.
+O valor das entradas deve ser calculado com base nas seguintes regras:
+• Segunda, terça e quinta, o valor base do ingresso é R$ 16,00.
+• Nas quartas todos pagam meia-entrada no valor de R$ 8,00, em qualquer horário.
+• Sexta, sábado e domingo, o valor base do ingresso é R$ 20,00.
+• Das 17h à meia-noite, há acréscimo de 50% no valor base do ingresso
+
